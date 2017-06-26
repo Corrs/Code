a05e7ea931a5d87c7d1e5c6bdb09de94380180ab
Corrs <1417200023@qq.com>
2017-06-26 10:02:18 +0800

    将main.js里的bootstrap引用去掉

--- awesomepos/src/main.js
+++ awesomepos/src/main.js
@@ -4,14 +4,11 @@ import Vue from 'vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
-import bootstrap from 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App'
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI);
-Vue.use(bootstrap);
 
 /* eslint-disable no-new */
 new Vue({
